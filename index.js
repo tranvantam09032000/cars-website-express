6390667769b0ab96cb1a7bb70244716ef4baba87
tranvantam09032000 <tranvantam09032000@gmail.com>
2025-03-05 06:12:59 +0700

    add socketIo chat realtime

--- index.js
+++ index.js
@@ -5,13 +5,31 @@ const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
+const chatRouters = require("./routes/chatRouters");
 const morgan = require("morgan");
 const logger = require("./logger/logger");
 const errorHandler = require("./middlewares/errorHandler");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 
 connectDB();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+    console.log("🟢 Client connected");
+
+    socket.on("chatMessage", (msg) => {
+        console.log("📩 Received message: ", msg);
+        io.emit("chatMessage", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("🔴 Client disconnected");
+    });
+});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,7 +39,6 @@ app.use(
     morgan(morganFormat, {
         stream: {
             write: (message) => {
-                console.log("run")
                 const logObject = {
                     method: message.split(" ")[0],
                     url: message.split(" ")[1],
@@ -37,7 +54,8 @@ app.use(
 app.use(errorHandler);
 
 app.use("/api/users", userRoutes);
+app.use("/chat", chatRouters);
 app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
